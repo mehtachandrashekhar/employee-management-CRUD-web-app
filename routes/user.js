c5a785807b6b79fe05c19c55177127dc81dce7ec
Chandrashekhar Mehta <mehtachandrashekhar84@gmail.com>
2024-04-02 21:34:36 +0530

    route setup

--- routes/user.js
+++ routes/user.js
@@ -1,29 +1,18 @@
-const express = require('express')
-const router = express.Router()
-const User = require('../models/user')
+const express = require('express');
+const { validationResult } = require('express-validator');
+const Employee = require('../models/Employee');
 
-router.post('/post', async (req, res) => {
-    try {
-        const { name, password } = req.body;
-        if (name === 'admin' && password === 'admin') {
-            return res.status(200).json('Welcome to the admin page')
-        }
+const router = express.Router();
 
-        if (!(name && password)) {
-            return res.status(200).json('Please enter password');
-        }
-        const checkUser = await User.findOne({ name, password });
-        if (checkUser) {
-            return res.status(409).json({ message: 'User already exist', });
-        }
-        const user = new User({ name, password });
-        await user.save();
-
-        return res.status(201).json({ message: 'User created Succesfully', user, })
-    } catch (error) {
-        console.log(error);
-        return res.status(500).json({ message: 'Intrenal Server error' })
-    }
+// Get all employees
+router.get('/employees', async (req, res) => {
+  try {
+    const employees = await Employee.find();
+    res.json(employees);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: 'Server error' });
+  }
 });
 
-module.exports = router
+module.exports = router;
