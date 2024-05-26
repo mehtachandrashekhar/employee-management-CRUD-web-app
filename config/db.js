976ccd92993d6f4688cb0afc5ec622d189528fbf
chandra <mehtachandrashekhar84@gmail.com>
2024-05-26 12:02:47 +0530

    updated config

--- config/db.js
+++ config/db.js
@@ -1,12 +1,11 @@
 const mongoose = require('mongoose');
-require('dotenv').config();
 
-const MONGODB_URI = process.env.MONGODB_URI;
+const url = 'mongodb://mongo:27017/docker-node-mongo';
 
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+const connectDb = () => {
+  mongoose.connect(url, () => {
+    console.log('Connected to MongoDB');
+  });
+};
 
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-db.once('open', () => {
-  console.log('Connected to the database');
-});
+module.exports = connectDb;
