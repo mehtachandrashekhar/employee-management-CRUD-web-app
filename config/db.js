1c5ad4bcb3e95caff045a5b98089469e77e42055
chandra <mehtachandrashekhar84@gmail.com>
2024-05-26 12:55:01 +0530

    updated db.js

--- config/db.js
+++ config/db.js
@@ -1,11 +1,16 @@
 const mongoose = require('mongoose');
 
-const url = 'mongodb://mongo:27017/docker-node-mongo';
-
-const connectDb = () => {
-  mongoose.connect(url, () => {
-    console.log('Connected to MongoDB');
-  });
+const connectDb = async () => {
+  try {
+    await mongoose.connect('mongodb://localhost:27017/myDatabase', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('MongoDB connected successfully');
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error);
+    process.exit(1);
+  }
 };
 
 module.exports = connectDb;
